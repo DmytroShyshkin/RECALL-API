c18724f373b05f2664b3ca643346ed01688103d3
Dmytro Shyshkin <dmytro.dev.shyshkin@gmail.com>
2026-09-10 13:49:30 +0200

    Add repository method to list user emails
    This commit adds a repository query that retrieves all user emails via JPQL from the Users entity. It enables code paths that need to inspect or validate registered email addresses across users.

--- src/main/java/com/dmytro/language_learning_api/repository/UsersRepository.java
+++ src/main/java/com/dmytro/language_learning_api/repository/UsersRepository.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.dmytro.language_learning_api.model.Users;
 
@@ -13,6 +14,7 @@ public interface UsersRepository extends JpaRepository<Users, UUID> {
     Optional<Users> findByEmail(String email);
     Optional<Users> findByUsername(String username);
 
+    @Query("SELECT u.email FROM Users u")
     List<String> findAllEmails();
 
     Optional<Users> findByVerificationToken(String token);
